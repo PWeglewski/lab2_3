bacc417ea302d41ce130f5275c7b9af5ddfdd7ce
Piotr Weglewski <190160@edu.p.lodz.pl>
2016-04-14 17:11:16 +0200

    Added test for non empty sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -29,7 +29,14 @@ public class SimilarityFinderTest {
 
         similarityFinder = new SimilarityFinder(new SequenceSearcher() {
             public SearchResult search(int i, int[] ints) {
-                final boolean isFound = Arrays.asList(ints).contains(i);
+                boolean foundFlag = false;
+                for (int j : ints){
+                    if(i==j){
+                        foundFlag = true;
+                        break;
+                    }
+                }
+                final boolean isFound = foundFlag;
 
                 invocations.add(new FuncInvocation(i, ints));
 
@@ -61,6 +68,30 @@ public class SimilarityFinderTest {
         assertThat(invocations.size(), is(equalTo(0)));
     }
 
+    @Test
+    public void shouldReturnCorrectResultForNonEmptySequences() throws Exception {
+        // given
+        int seqOne[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int seqTwo[] = {1, 3, 5, 7, 9};
+
+        // when
+        double result = similarityFinder.calculateJackardSimilarity(seqOne, seqTwo);
+
+        // then
+        assertThat(result, is(equalTo(0.5d)));
+        assertThat(invocations.size(), is(equalTo(seqOne.length)));
+        assertThat(countInvocations(1, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(2, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(3, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(4, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(5, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(6, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(7, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(8, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(9, seqTwo), is(equalTo(1)));
+        assertThat(countInvocations(10, seqTwo), is(equalTo(1)));
+    }
+
     class FuncInvocation {
         public int[] seq;
         int i;
@@ -69,5 +100,16 @@ public class SimilarityFinderTest {
             this.i = i;
             this.seq = seq;
         }
+    }
+
+    public int countInvocations(int i, int[] seq){
+        int count = 0;
+
+        for (FuncInvocation invocation : invocations){
+            if(invocation.i==i&&invocation.seq==seq)
+                count++;
+        }
+
+        return count;
     }
 }
